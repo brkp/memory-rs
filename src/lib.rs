96f565cb3e2087e2aea62c032981d21fd7b1286e
brkp <brkp@tutanota.com>
2022-12-08 18:35:35 +0300

    error/result stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,27 @@ mod platform;
 mod process;
 mod region;
 
-pub mod prelude {
-    pub use super::platform::Pid;
+#[cfg(target_os = "linux")]
+mod common {
+    pub use super::platform::linux::Pid;
 }
+
+#[cfg(target_os = "windows")]
+mod common {
+    pub use super::platform::linux::Pid;
+}
+
+pub use common::*;
+
+pub enum Error {
+    ProcessNotFound,
+    OsError(std::io::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::OsError(err)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
