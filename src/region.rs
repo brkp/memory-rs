f97fdb95f2a47bf27fa7b5b662f2ef526f0fbd8f
brkp <brkp@tutanota.com>
2022-12-08 20:26:08 +0300

    make MemoryRegion fields public

--- src/region.rs
+++ src/region.rs
@@ -1,10 +1,10 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct MemoryRegion {
-    base: *const u8,
-    size: usize,
-    exec: bool,
-    read: bool,
-    write: bool,
+    pub base: *const u8,
+    pub size: usize,
+    pub exec: bool,
+    pub read: bool,
+    pub write: bool,
 }
 
 impl MemoryRegion {
