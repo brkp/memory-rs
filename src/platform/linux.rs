4332d5832a25fc447a63a9b7f565e501423cee2a
brkp <brkp@tutanota.com>
2022-12-08 19:00:46 +0300

    `Process::new` for linux

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -2,4 +2,22 @@ use crate::process::Process;
 
 pub type Pid = libc::pid_t;
 
-impl Process {}
+impl Process {
+    pub fn new(pid: Pid) -> crate::Result<Self> {
+        if std::path::Path::new(&format!("/proc/{}", pid)).exists() {
+            Ok(Self { pid })
+        } else {
+            Err(crate::Error::ProcessNotFound)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_process_self() {
+        assert!(Process::new(std::process::id() as Pid).is_ok());
+    }
+}
