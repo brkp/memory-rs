14519224c382e8ffd85e065480f4dd625c714e1d
brkp <brkp@tutanota.com>
2022-12-08 18:34:56 +0300

    linux module

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -1,1 +1,5 @@
+use crate::process::Process;
+
 pub type Pid = libc::pid_t;
+
+impl Process {}
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,11 +1,5 @@
 #[cfg(target_os = "linux")]
-mod linux;
+pub mod linux;
 
 #[cfg(target_os = "windows")]
-mod windows;
-
-#[cfg(target_os = "linux")]
-pub use linux::*;
-
-#[cfg(target_os = "windows")]
-pub use windows::*;
+pub mod windows;
