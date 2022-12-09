0981529b2017414ae5292e28e1acde08d340f652
brkp <brkp@tutanota.com>
2022-12-09 17:14:56 +0300

    Process::new implementation for windows

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,1 +1,50 @@
+use crate::{MemoryRegion, Process};
+use windows::Win32::{
+    Foundation::{NTSTATUS, STILL_ACTIVE},
+    System::Threading::GetExitCodeProcess,
+};
+pub use windows::{
+    core::{Error, Result, HRESULT},
+    Win32::{
+        Foundation::{
+            CloseHandle, GetLastError, ERROR_ACCESS_DENIED, ERROR_INVALID_PARAMETER, HANDLE,
+        },
+        System::{
+            Diagnostics::Debug::ReadProcessMemory,
+            Memory::{
+                VirtualQueryEx, MEMORY_BASIC_INFORMATION, MEM_COMMIT, MEM_MAPPED, MEM_PRIVATE,
+            },
+            Threading::{
+                OpenProcess, PROCESS_ACCESS_RIGHTS, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ,
+            },
+        },
+    },
+};
+
 pub type Pid = u32;
+
+impl Process {
+    pub fn new(pid: Pid) -> crate::Result<Self> {
+        match unsafe { OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, false, pid) } {
+            Ok(handle) => {
+                let mut exit_code = 0;
+                if unsafe { GetExitCodeProcess(handle, &mut exit_code).as_bool() } {
+                    if NTSTATUS(exit_code as i32) == STILL_ACTIVE {
+                        return Ok(Self { pid, handle });
+                    }
+                }
+
+                unsafe { CloseHandle(handle) };
+                return Err(crate::Error::ProcessNotFound);
+            }
+            Err(err) => {
+                if err.code() == ERROR_INVALID_PARAMETER.to_hresult() {
+                    return Err(crate::Error::ProcessNotFound);
+                }
+
+                let err: std::io::Error = err.into();
+                return Err(crate::Error::from(err));
+            }
+        }
+    }
+}
